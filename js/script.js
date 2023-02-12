844842f32f6b224591cc8165243c42f772d883b3
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-02-12 16:06:46 +0600

    changes many things

--- js/script.js
+++ js/script.js
@@ -1,41 +1,56 @@
 const balanceOutPut = document.getElementById("balance");
 const depositInput = document.getElementById("deposit-input");
 
-
-
 let defaultBalance = 1200;
-balanceOutPut.innerText = `$${defaultBalance}`;
+balanceOutPut.innerText = `${defaultBalance}`;
 
 /**
- * ! deposit functionality 
+ * ! deposit functionality
  */
 
 document.getElementById("deposit-button").addEventListener("click", () => {
-  let depositAmount = parseInt(depositInput.value);
-  let previousDepositAmount = parseInt(
-    document.getElementById("deposit").innerText
-  );
-  document.getElementById("deposit").innerText = depositAmount +=
-    previousDepositAmount;
+  if (depositInput.value) {
+    let depositAmount = parseFloat(depositInput.value);
+    let previousDepositAmount = parseFloat(
+      document.getElementById("deposit").innerText
+    );
+    document.getElementById("deposit").innerText = (depositAmount +=
+      previousDepositAmount).toFixed(2);
 
-  defaultBalance += parseInt(depositInput.value);
-  balanceOutPut.innerText = `$${defaultBalance}`;
-  depositInput.value = "";
+    defaultBalance += parseFloat(depositInput.value);
+    balanceOutPut.innerText = `${defaultBalance}`;
+    depositInput.value = "";
+  } else {
+    console.log("input required");
+  }
 });
 
-
 /**
- * ! Withdraw functionality 
+ * ! Withdraw functionality
  */
 document.getElementById("withdraw-button").addEventListener("click", () => {
   let withdrawInput = document.getElementById("withdraw-input");
-  balanceOutPut.innerText = defaultBalance - withdrawInput.value;
-  let previousWithdrawAmount = parseInt(
-    document.getElementById("withdraw").innerText
-  );
 
-  previousWithdrawAmount += parseInt(withdrawInput.value);
-  document.getElementById("withdraw").innerText = previousWithdrawAmount;
+  if (withdrawInput.value) {
+    if (parseFloat(balanceOutPut.innerText) < parseFloat(withdrawInput.value)) {
+      document.getElementById("withdrawWarning").style.visibility = "visible";
+    } else {
+      balanceOutPut.innerText =
+        parseFloat(balanceOutPut.innerText) - parseFloat(withdrawInput.value);
 
-  withdrawInput.value = "";
+      let previousWithdrawAmount = parseFloat(
+        document.getElementById("withdraw").innerText
+      );
+
+      previousWithdrawAmount += parseFloat(withdrawInput.value);
+      document.getElementById("withdraw").innerText =
+        previousWithdrawAmount.toFixed(2);
+    }
+
+    withdrawInput.value = "";
+  } else {
+    document.getElementById("withdrawWarning").innerText =
+      "Please enter the amount";
+    document.getElementById("withdrawWarning").style.visibility = "visible";
+  }
 });
